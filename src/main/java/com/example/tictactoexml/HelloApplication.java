a9b1be1809f8519f617b5178f13b6e4d20bef7b4
a20antchoand <a20antchoand@inspedralbes.cat>
2022-01-21 14:17:27 +0100

    dafg

--- src/main/java/com/example/tictactoexml/HelloApplication.java
+++ src/main/java/com/example/tictactoexml/HelloApplication.java
@@ -10,28 +10,21 @@ import java.util.Random;
 
 public class HelloApplication extends Application {
 
-    static Random rand = new Random();
-    static HelloController control = new HelloController();
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("TicTacToe");
         stage.setScene(scene);
         stage.show();
-
-        int validarTurn = rand.nextInt(2);
-        control.validarTurn = validarTurn;
-        control.primerTurn();
-
     }
 
     public static void main(String[] args) {
 
         launch();
 
-
+        HelloController hc = new HelloController();
 
     }
 }
--- src/main/java/com/example/tictactoexml/HelloController.java
+++ src/main/java/com/example/tictactoexml/HelloController.java
@@ -4,50 +4,127 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 import java.util.Random;
 
 public class HelloController {
 
+    static Random random = new Random();
+    static boolean turnActual;
+    static Button[] btn = new Button[9];
+    static HelloApplication joc;
+    static int chance_flag = 0;
 
     @FXML
-    private Label welcomeText;
+    static private Label turno;
+
     @FXML
-    private Label turn;
+    static private Button btn0_0;
+    @FXML
+    static private Button btn0_1;
+    @FXML
+    static private Button btn0_2;
+    @FXML
+    static private Button btn1_0;
+    @FXML
+    static private Button btn1_1;
+    @FXML
+    static private Button btn1_2;
+    @FXML
+    static private Button btn2_0;
+    @FXML
+    static private Button btn2_1;
+    @FXML
+    static private Button btn2_2;
 
-    public int validarTurn;
-
-    protected void primerTurn() {
-
-        System.out.println("Hola");
-
-        if (validarTurn == 0) {
-            System.out.println("Hola");
-
-            turn.setText("TURN DE: X");
+    @FXML
+    protected void btn_onClick(ActionEvent event) {
+        chance_flag++;
+        if (turnActual) {
+            turnActual = false;
+            turno.setText("TURN DE: O");
         } else {
-            System.out.println("Hola");
-
-            turn.setText("TURN DE: O");
+            turnActual = true;
+            turno.setText("TURN DE: X");
         }
-
     }
 
-    @FXML
-    protected void onHelloButtonClick(ActionEvent event) {
-        System.out.println(validarTurn);
 
-        if (validarTurn == 0) {
 
-        turn.setText("TURN DE: X");
-            ((Button)event.getSource()).setText("O");
-            validarTurn = 1;
+    public static void HelloController() {
+
+        btn[0] = btn0_0;
+        btn[1] = btn0_1;
+        btn[2] = btn0_2;
+        btn[3] = btn1_0;
+        btn[4] = btn1_1;
+        btn[5] = btn2_2;
+        btn[6] = btn0_0;
+        btn[7] = btn1_1;
+        btn[8] = btn2_2;
+
+        int chance = random.nextInt(100);
+        if (chance % 2 == 0) {
+            turnActual = true;
+            turno.setText("TURN DE: X");
         } else {
-            turn.setText("TURN DE: O");
-            ((Button)event.getSource()).setText("X");
-            validarTurn = 0;
+            turnActual = false;
+            turno.setText("TURN DE: O");
+        }
+    }
+
+    public void guanyador() {
+        if ((btn[0].getText() == "X") && (btn[1].getText() == "X") && (btn[2].getText() == "X")) {
+            xGuanya(0, 1, 2);
+        } else if ((btn[0].getText() == "X") && (btn[4].getText() == "X") && (btn[8].getText() == "X")) {
+            xGuanya(0, 4, 8);
+        } else if ((btn[0].getText() == "X") && (btn[3].getText() == "X") && (btn[6].getText() == "X")) {
+            xGuanya(0, 3, 6);
+        } else if ((btn[1].getText() == "X") && (btn[4].getText() == "X") && (btn[7].getText() == "X")) {
+            xGuanya(1, 4, 7);
+        } else if ((btn[2].getText() == "X") && (btn[4].getText() == "X") && (btn[6].getText() == "X")) {
+            xGuanya(2, 4, 6);
+        } else if ((btn[2].getText() == "X") && (btn[5].getText() == "X") && (btn[8].getText() == "X")) {
+            xGuanya(2, 5, 8);
+        } else if ((btn[3].getText() == "X") && (btn[4].getText() == "X") && (btn[5].getText() == "X")) {
+            xGuanya(3, 4, 5);
+        } else if ((btn[6].getText() == "X") && (btn[7].getText() == "X") && (btn[8].getText() == "X")) {
+            xGuanya(6, 7, 8);
         }
 
-
+        else if ((btn[0].getText() == "O") && (btn[1].getText() == "O") && (btn[2].getText() == "O")) {
+            oGuanya(0, 1, 2);
+        } else if ((btn[0].getText() == "O") && (btn[3].getText() == "O") && (btn[6].getText() == "O")) {
+            oGuanya(0, 3, 6);
+        } else if ((btn[0].getText() == "O") && (btn[4].getText() == "O") && (btn[8].getText() == "O")) {
+            oGuanya(0, 4, 8);
+        } else if ((btn[1].getText() == "O") && (btn[4].getText() == "O") && (btn[7].getText() == "O")) {
+            oGuanya(1, 4, 7);
+        } else if ((btn[2].getText() == "O") && (btn[4].getText() == "O") && (btn[6].getText() == "O")) {
+            oGuanya(2, 4, 6);
+        } else if ((btn[2].getText() == "O") && (btn[5].getText() == "O") && (btn[8].getText() == "O")) {
+            oGuanya(2, 5, 8);
+        } else if ((btn[3].getText() == "O") && (btn[4].getText() == "O") && (btn[5].getText() == "O")) {
+            oGuanya(3, 4, 5);
+        } else if ((btn[6].getText() == "O") && (btn[7].getText() == "O") && (btn[8].getText() == "O")) {
+            oGuanya(6, 7, 8);
+        } else if (chance_flag == 9) {
+            turno.setText("EMPAT");
+        }
     }
+
+    public void xGuanya(int x1, int x2, int x3) {
+        turno.setText("X HA GUANYAT");
+    }
+
+    public void oGuanya(int x1, int x2, int x3) {
+        turno.setFont(new Font("Apple Casual", 50));
+        turno.setText("O HA GUANYAT");
+    }
+    
+    
+    
+    
 }
